bf2db888596a463ff726c200438db9aab96ddff5
Walter Junior <walterjuniorgomes091@gmail.com>
2022-04-20 10:54:03 -0300

    Concluido o bruto até a modificação dos arquivos

--- automacaoTeste/__init__.py
+++ automacaoTeste/__init__.py
@@ -2,9 +2,5 @@ import autoTesteBase
 import autoExplorer
 
 autoExplorer.openExplorerFile()
-
 # autoTesteBase.localizationPositionMouse()
 
-# Point(x=524, y=133)
-# Point(x=532, y=157)
-# Point(x=459, y=172)
--- automacaoTeste/autoExplorer.py
+++ automacaoTeste/autoExplorer.py
@@ -1,37 +1,30 @@
 import pyautogui
 import time
-
 from pyperclip import paste
 
-CONT_DOWN_FILE = 1;
-
 def openExplorerFile():
-
 
     # Mostrar mensagem de Alerta sobre o inicio da automação
     pyautogui.alert("Automação iniciada. Por favor, não use o teclado "
                     "e nem mecha no mouse, para evitar erros!")
-
-    #Fechando possivéis janelas
-
-
 
     #Abrindo a Aréa de Trabalho
     pyautogui.hotkey("winleft", "d")
 
     #Abrindo o Explorador de Arquivos
     pyautogui.press("winleft")
-    time.sleep(1)
+    time.sleep(1.5)
     pyautogui.write("Explorador de Arquivos")
-    time.sleep(2)
+    time.sleep(5)
     pyautogui.press("enter")
 
     #Maximizando a tela
-    time.sleep(2)
+    time.sleep(2.5)
     pyautogui.moveTo(x=858, y=152)
     pyautogui.leftClick()
 
     #Acessando e informando o caminho do arquivo zip
+    time.sleep(1.5)
     pyautogui.moveTo(x=615, y=68)
     pyautogui.leftClick()
     time.sleep(2)
@@ -47,21 +40,30 @@ def openExplorerFile():
     pyautogui.press("enter")
 
     #Selecionando e abrindo o arquivo .zip
-    time.sleep(2)
+    time.sleep(5.5)
     pyautogui.moveTo(x=502, y=163)
     pyautogui.doubleClick()
 
-    while True:
-        if (abrirArquivo(1)):
-            CONT_DOWN_FILE + 1
-            # salvarArquivo()
+    entra = True
+    indice = 1
+    while entra:
+        if(abrirArquivo(indice)):
+            time.sleep(5)
+            salvarArquivo(indice)
+            time.sleep(2)
+            closeFile()
+            backToExplorer()
+            indice = indice + 1
+            entra = True
         else:
             pyautogui.alert("Impossivel Abrir arquivos .docx ou arquivos que não começam com um Númeral!!")
+            continue
 
 
 
 def abrirArquivo(indice):
     if (indice == 1):
+        conteudo = None
         pyautogui.press("down", 1)
         pyautogui.press("up")
         pyautogui.moveTo(x=524, y=133)
@@ -71,46 +73,68 @@ def abrirArquivo(indice):
         pyautogui.hotkey('ctrl', 'c') #Copia conteúdo selecionado
         conteudo = paste()
         pyautogui.typewrite(conteudo)  # Digita o texto da variável conteúdo
-        pyautogui.alert(conteudo)
         time.sleep(5)
         file = str(conteudo)
+        # setNameFile(file)
         if (validationArquivo(file)):
+            time.sleep(3)
             pyautogui.press("enter")
-            pyautogui.press("esc")
-            pyautogui.doubleClick()
+            time.sleep(1.5)
+            pyautogui.press("enter")
             return True
         else:
             return False
+
     else:
-        pyautogui.press("down", CONT_DOWN_FILE)
-        pyautogui.press("up")
-        pyautogui.press("enter")
+        conteudo = None
+        pyautogui.press("down", 1)
+        # pyautogui.press("up", 1)
+        # pyautogui.press("enter")
         pyautogui.press("apps")
         pyautogui.press("down", 5)
         pyautogui.press("enter")
         pyautogui.hotkey('ctrl', 'c') #Copia conteúdo selecionado
         conteudo = paste()
         pyautogui.typewrite(conteudo)  # Digita o texto da variável conteúdo
-        pyautogui.alert(conteudo)
-        time.sleep(5)
+        time.sleep(2)
         file = str(conteudo)
+        # setNameFile(file)
         if (validationArquivo(file)):
+            time.sleep(3)
             pyautogui.press("enter")
-            pyautogui.press("esc")
-            pyautogui.doubleClick()
+            time.sleep(1.5)
+            pyautogui.press("enter")
             return True
         else:
             return False
 
-# def salvarArquivo():
-#     pyautogui.press("apps")
+def salvarArquivo(indice):
+    time.sleep(5)
+    pyautogui.moveTo(x=31, y=50)
+    pyautogui.leftClick()
+    time.sleep(2)
+    pyautogui.moveTo(x=83, y=239)
+    pyautogui.leftClick()
+    time.sleep(2)
+    pyautogui.write("Pagina " + str(indice)
+                    + " - Modificado")
+
+    pyautogui.press("enter")
+
+def backToExplorer():
+    pyautogui.hotkey("alt", "tab")
+    time.sleep(3)
+
+def closeFile():
+    pyautogui.moveTo(x=345, y=11)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(2)
 
 def validationArquivo(file):
     if (file[0].isnumeric()):
+
         if(file.__contains__(".pdf")):
             return True
     else:
         return False
-
-
-
