faf274236b48fccd1a998503b18f9d9375bfa763
Walter Junior <walterjuniorgomes091@gmail.com>
2022-04-25 22:31:21 -0300

    commitMsg

--- automacaoTeste/AutoBase.py
+++ automacaoTeste/AutoBase.py
@@ -68,3 +68,56 @@ def mesclarCelulas(planilha, celulas, linhaInicial, linhaFinal, colunaInicial, c
 def moverPlanilha(caminho, novoCaminho):
     shutil.move(caminho, novoCaminho)
     print("Planilha movida")
+
+# ------------------------------------------------ git
+
+class git():
+
+    def addCommitAnPush(self):
+        self.abrirProjectAndGit()
+        self.addA()
+        print("Add Sucess")
+        self.abrirProjectAndGit()
+        self.commit()
+        print("Commit Sucess")
+        self.abrirProjectAndGit()
+        self.push()
+        print("Push Sucess")
+
+    def abrirProjectAndGit(self):
+        py.moveTo(x=88, y=93)
+        py.leftClick()
+        py.rightClick()
+        time.sleep(1)
+        py.press("down", 20)
+        time.sleep(1)
+        py.press("right")
+
+    def addA(self):
+        time.sleep(1)
+        py.press("down")
+        time.sleep(1)
+        py.press("enter")
+        time.sleep(5)
+
+    def commit(self):
+        time.sleep(1)
+        py.press("enter")
+        time.sleep(1)
+        py.write("commitMsg")
+        time.sleep(1)
+        py.moveTo(x=73, y=430)
+        py.leftClick()
+        time.sleep(5)
+        py.moveTo(x=345, y=71)
+        py.leftClick()
+        py.moveTo(x=10, y=83)
+        py.leftClick()
+        time.sleep(5)
+
+    def push(self):
+        py.moveTo(x=608, y=293)
+        py.leftClick()
+        time.sleep(1)
+        py.moveTo(x=717, y=569)
+        py.leftClick()
--- automacaoTeste/__init__.py
+++ automacaoTeste/__init__.py
@@ -6,56 +6,6 @@ import time
 # obj.Index()
 # AutoBase.localizationPositionMouse()
 
-
-py.moveTo(x=88, y=93)
-py.leftClick()
-py.rightClick()
-time.sleep(1)
-py.press("down", 20)
-time.sleep(1)
-
-py.press("right")
-time.sleep(1)
-py.press("down")
-time.sleep(1)
-py.press("enter")
-time.sleep(5)
-
-py.moveTo(x=88, y=93)
-py.leftClick()
-py.rightClick()
-time.sleep(1)
-py.press("down", 20)
-time.sleep(1)
-
-py.press("right")
-time.sleep(1)
-py.press("enter")
-time.sleep(1)
-py.write("commitMsg")
-time.sleep(1)
-py.moveTo(x=73, y=430)
-py.leftClick()
-time.sleep(5)
-
-py.moveTo(x=345, y=71)
-py.leftClick()
-py.moveTo(x=10, y=83)
-py.leftClick()
-time.sleep(5)
-
-py.moveTo(x=88, y=93)
-py.leftClick()
-py.rightClick()
-time.sleep(1)
-py.press("down", 20)
-time.sleep(1)
-py.press("right")
-
-py.moveTo(x=608, y=293)
-py.leftClick()
-time.sleep(1)
-py.moveTo(x=717, y=569)
-py.leftClick()
-
+git = AutoBase.git()
+git.addCommitAnPush()
 
